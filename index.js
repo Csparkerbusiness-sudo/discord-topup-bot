a103de61955b4be33436000f1aa9684b1558c3de
Csparkerbusiness-sudo <paypament.business@gmail.com>
2026-05-29 12:14:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 require('dotenv').config();
+
+const express = require('express');
 const { Client, GatewayIntentBits } = require('discord.js');
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 client.once('ready', () => {
